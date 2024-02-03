b9bc59c492afb217ab3ba53e5780711375c420db
azurecurve <github@azurecurve.co.uk>
2024-02-03 15:49:57 +0000

    Release v1.2.8

--- azrcrv-theme-switcher.php
+++ azrcrv-theme-switcher.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Theme Switcher
- * Description: Allows users to easily switch themes (ideal for allowing light/dark mode).
- * Version: 1.2.7
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/theme-switcher/
- * Text Domain: theme-switcher
- * Domain Path: /languages
+ * Plugin Name:		Theme Switcher
+ * Description:		Allows users to easily switch themes (ideal for allowing light/dark mode).
+ * Version:			1.2.8
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/theme-switcher/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain: 	theme-switcher
+ * Domain Path: 	/assets/languages
+ * License: 		GPLv2 or later
+ * License URI: 	http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
